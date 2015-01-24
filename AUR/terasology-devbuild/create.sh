5a11aa303a8c43d8902003efb2f07d13f65d4237
Max Bruckner <max@maxbruckner.de>
2015-01-24 09:26:58 +0100

    terasology-devbuild: add create.sh

---
+++ AUR/terasology-devbuild/create.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ ! $# -eq "1" ]]; then
+	echo "Usage:"
+	echo "$0 version"
+	exit 1
+fi
+
+rm -v Terasology.zip
+echo "Setting version to $1"
+sed -i "s/pkgver=.*/pkgver=$1/g" PKGBUILD
+echo updpkgsums
+updpkgsums
+chmod -v 644 PKGBUILD
+echo mkaurball -f
+mkaurball -f
