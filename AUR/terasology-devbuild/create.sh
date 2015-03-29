348956ed8c111715dc906a53ed62af1dce84bf0b
Max Bruckner <max@maxbruckner.de>
2015-03-29 17:35:23 +0200

    Terasology: update create.sh for Omega builds

--- AUR/terasology-devbuild/create.sh
+++ AUR/terasology-devbuild/create.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-if [[ ! $# -eq "1" ]]; then
+if [[ ! $# -eq "2" ]]; then
 	echo "Usage:"
-	echo "$0 version"
+	echo "$0 version omege_version"
 	exit 1
 fi
 
-rm -v Terasology.zip{,.part}
+rm -v TerasologyOmega.zip{,.part}
 rm -rv src/
+
 echo "Setting version to $1"
 sed -i "s/pkgver=.*/pkgver=$1/g" PKGBUILD
+
+echo "Setting Omega version to $2"
+sed -i "s/_omega_ver=.*/_omega_ver=$2/g" PKGBUILD
+
 echo updpkgsums
 updpkgsums
 chmod -v 644 PKGBUILD
